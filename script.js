479ebac99e616ae944244431ffda4ff4586335b7
Dante Ortiz <verlacdante@gmail.com>
2023-08-04 16:49:51 -0700

    v1.0 html and css done

---
+++ script.js
@@ -0,0 +1,4 @@
+const form = document.getElementById('form');
+const fileInput = document.getElementById('file-input');
+const addressOutput = document.getElementById('url-output');
+const closeBtn = document.getElementById('close-btn');
